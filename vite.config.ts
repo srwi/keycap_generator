b4defa3cb51f760fa55202ec18ab909054b04468
Stephan Rumswinkel <17520641+srwi@users.noreply.github.com>
2025-12-28 23:33:11 +0100

    Deploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/keycap_generator/' : '/',
   plugins: [svelte()],
 })
