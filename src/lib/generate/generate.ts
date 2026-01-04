b8f6f1ccc0ca1b81e6816fbf4b9138f1791e722b
Stephan Rumswinkel <17520641+srwi@users.noreply.github.com>
2026-01-04 22:38:51 +0100

    Fix text extrusion depth

--- src/lib/generate/generate.ts
+++ src/lib/generate/generate.ts
@@ -89,7 +89,7 @@ export async function generateKeycapModel(
     const textGeom = new TextGeometry(text, {
       font,
       size: sym.fontSizeMm,
-      height: state.settings.extrusionDepthMm,
+      depth: state.settings.extrusionDepthMm,
       curveSegments: 6,
       bevelEnabled: false,
     } as any)
